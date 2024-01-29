680f6a5cc1976b839fb0a88ebf3fde5eacd0b1d6
Thiel <patrickthiel@outlook.de>
2024-01-29 16:03:01 +0100

    Create prepare_mannheim_districts.R
    Function to clean the city districts of Mannheim.

---
+++ src/prepare_mannheim_districts.R
@@ -0,0 +1,51 @@
+prepare_mannheim_districts <- function(utmcrs = NA) {
+    #' @title Preparation of city districts of Mannheim
+    #' 
+    #' @param utmcrs UTM (32632) projection
+    #' 
+    #' @return Spatial Dataframe with city districts
+    #' @author Patrick Thiel
+    
+    #----------------------------------------------
+    # read data
+
+    mannheim_districts <- sf::st_read(
+        file.path(
+            paths()[["data_path"]],
+            "raw_data",
+            "provided_geo_info",
+            "Stadtteile",
+            "Stadtteile_Mannheim.shp"
+        ),
+        quiet = TRUE
+    )
+
+    #----------------------------------------------
+    # clean data
+
+    mannheim_districts <- mannheim_districts |>
+        dplyr::select(
+            city_district = name,
+            city_district_id = id,
+            geometry
+        ) |>
+        sf::st_transform(crs = utmcrs)
+
+    #----------------------------------------------
+    # export
+
+    qs::qsave(
+        mannheim_districts,
+        file.path(
+            paths()[["data_path"]],
+            "processed_data",
+            "geo_information",
+            "mannheim_city_districts.qs"
+        )
+    )
+
+    #----------------------------------------------
+    # return
+
+    return(mannheim_districts)
+}
