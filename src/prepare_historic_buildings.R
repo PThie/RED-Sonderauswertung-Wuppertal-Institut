fa4e7903cd5662e215cf79c43015092ba35787d5
Thiel <patrickthiel@outlook.de>
2024-02-06 13:54:52 +0100

    New data: House shapes
    Adjust the cleaning according to the new data source which represents building shapes.
    
    Generate also a unique ID because the given ID is non-unique and there are multiple buildings with the same shape/ attributes but a different address.

--- src/prepare_historic_buildings.R
+++ src/prepare_historic_buildings.R
@@ -17,8 +17,7 @@ prepare_historic_buildings <- function(utmcrs = NA) {
             paths()[["data_path"]],
             "raw_data",
             "protected_buildings",
-            "Baudenkmaeler_EPSG3857_SHAPE",
-            "Baudenkmaeler_EPSG3857_SHAPE.shp"
+            "Objekte_denkmalschutz_Wuppertal.gpkg"
         ),
         quiet = TRUE
     )
@@ -26,25 +25,44 @@ prepare_historic_buildings <- function(utmcrs = NA) {
     #----------------------------------------------
     # clean data
 
+    # set geometry columns
+    sf::st_geometry(historic_buildings) <- "geometry"
+
+    # select only ID and geometry
     historic_buildings <- historic_buildings |>
-        dplyr::select(
-            historic_building_id = NR_DENKMAL,
-            geometry
+        dplyr::mutate(
+            url_nr = stringr::str_extract(X_impURL, "\\d+"),
+            historic_building_id = paste0(UUID, url_nr, X_impBEZEIC, X_impSTRASS, X_impUMFANG),
+            historic_building_id = stringr::str_replace_all(historic_building_id, " ", ""),
+            historic_building_id = stringr::str_replace_all(historic_building_id, "\\.", ""),
+            historic_building_id = stringr::str_replace_all(historic_building_id, "-", ""),
+            historic_building_id = stringi::stri_trans_general(historic_building_id, "DE-ASCII; Latin-ASCII"),
         ) |>
         sf::st_transform(crs = utmcrs)
-
+    
     #----------------------------------------------
     # export
 
-    qs::qsave(
+    sf::st_write(
         historic_buildings,
         file.path(
             paths()[["data_path"]],
             "processed_data",
             "geo_information",
-            "wuppertal_historic_buildings.qs"
-        )
+            "wuppertal_historic_buildings.gpkg"
+        ),
+        quiet = TRUE,
+        append = FALSE
     )
+
+    #----------------------------------------------
+    # keep only ID and geometry for further processing
+
+    historic_buildings <- historic_buildings |>
+        dplyr::select(
+            historic_building_id,
+            geometry
+        )
 
     #----------------------------------------------
     # return
